a17cdec107cffaa85c996aeead0545c8b575c960
corpbob <me@example.com>
2019-04-02 12:18:29 -0400

    testing

--- routes/fortune.js
+++ routes/fortune.js
@@ -2,6 +2,8 @@ child_process = require('child_process');
 var express = require('express');
 var router = express.Router();
 var command = process.env.FORTUNE_COMMAND;
+var host=process.env.HOST
+var port=process.env.PORT
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,15 @@ router.get('/', function(req, res, next) {
     command="fortune";
   }
  
+  console.log("executing command: " + command);
+  var fortune = child_process.execSync(command);
+  res.send(fortune.toString() + '\n');
+});
+
+router.get('/test', function(req, res, next) {
+  console.log(req.headers);
+  user_agent=req.headers['user-agent'];
+  command= 'curl -v -H ' + '"User-Agent: ' + user_agent + '" http://' + host + ':' + port + '/fortune';
   console.log("executing command: " + command);
   var fortune = child_process.execSync(command);
   res.send(fortune.toString() + '\n');
